d57079b245be057187cc51c391b2c73de8d8c240
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2019-04-28 19:46:31 +0200

    Newly created stacks should be empty.

---
+++ stack/src/main/java/com/soat/kata/Stack.java
@@ -0,0 +1,7 @@
+package com.soat.kata;
+
+public class Stack {
+    public int getSize() {
+        return 0;
+    }
+}
--- stack/src/test/java/StackShould.java
+++ stack/src/test/java/StackShould.java
@@ -1,11 +1,16 @@
+import com.soat.kata.Stack;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StackShould {
 
-    @Test
-    public void works() {
-        Assertions.assertThat(true).isTrue();
-    }
+    private static final int DEFAULT_STACK_SIZE_WHEN_CREATED = 0;
 
+    @Test
+    public void be_empty_when_created() {
+        Stack stack  = new Stack();
+        assertThat(stack.getSize()).isEqualTo(DEFAULT_STACK_SIZE_WHEN_CREATED);
+    }
 }
