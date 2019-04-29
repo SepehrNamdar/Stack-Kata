6f72d22187a9bae35188ae839b0356f1233d522f
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2019-04-29 15:24:55 +0200

    After one push, stack size should be one

--- stack/src/test/java/StackShould.java
+++ stack/src/test/java/StackShould.java
@@ -1,15 +1,27 @@
 import com.soat.kata.Stack;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StackShould {
 
+    private Stack stack;
+
+    @Before
+    public void init() {
+        stack = new Stack();
+    }
+
+
     @Test
     public void be_empty_when_created() {
-        Stack stack = new Stack();
-
         assertThat(stack.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void has_a_size_of_one_after_one_push() {
+        assertThat(stack.getSize()).isEqualTo(1);
     }
 }
